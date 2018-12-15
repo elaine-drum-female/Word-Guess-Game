8ebd64fcaab581f32defe8557f2ccdcf239e9d05
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2018-12-14 21:19:16 -0600

    loses guesses remaining and wins element text content added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -50,76 +50,78 @@ function startGame() {
     // Capture the keys fired by the user
     document.onkeyup = function (event) {
 
-        lettersGuessedElement = event.key;
-        //console.log(lettersGuessedElement);
+            lettersGuessedElement = event.key;
+            //console.log(lettersGuessedElement);
 
 
-        //check to see if value from the users selection is of the first occurence if it is NOT more than - 1 than have the computer's index equal the same of the lettersguessed
-        if (computerSelectsRandomElement.indexOf(lettersGuessedElement) > -1) {
-            for (var i = 0; i < computerSelectsRandomElement.length; i++) {
-                if (computerSelectsRandomElement[i] === lettersGuessedElement) {
-                    underScore[i] = lettersGuessedElement;
-                    wordBox.textContent = underScore.join(' ');
+            //check to see if value from the users selection is of the first occurence if it is NOT more than - 1 than have the computer's index equal the same of the lettersguessed
+            if (computerSelectsRandomElement.indexOf(lettersGuessedElement) > -1) {
+                for (var i = 0; i < computerSelectsRandomElement.length; i++) {
+                    if (computerSelectsRandomElement[i] === lettersGuessedElement) {
+                        underScore[i] = lettersGuessedElement;
+                        wordBox.textContent = underScore.join(' ');
+                    }
                 }
             }
-        }
 
 
-        function randomImage() {
-            var randomImage = document.getElementById('imgSelect');
-            if (computerSelectsRandomElement == "demi") {
-                randomImage.src = "demi-lovato.jpg";
-            } else if (computerSelectsRandomElement == "band-pose") {
-                randomImage.src = "band-pose2";
-            }
-        }
-
-        randomImage();
-
-
-    // start the RIGHT LETTER FOR LOOP
-    for (var w = 0; w < songTitle.length; w++) {
-
-        if (lettersGuessedElement == songTitle[w]) {
-            correct = true;
-            rightLetter.push(lettersGuessedElement);
-            wins++;
-        }
-
-    // Searching for duplicates
-
-    var count = 0;
-    var found = false;
-
-
-    for (r = 0; r < guessLetters.length; r++) {
-        //console.log(guessLetters[i]);
-        for (c = 0; c < removeDuplicates.length; c++) {
-            if (guessLetters[c] == removeDuplicates[c]) {
-                found = true;
-
+            function randomImage() {
+                var randomImage = document.getElementById('imgSelect');
+                if (computerSelectsRandomElement == "demi") {
+                    randomImage.src = "demi-lovato.jpg";
+                } else if (computerSelectsRandomElement == "band-pose") {
+                    randomImage.src = "band-pose2";
+                }
             }
 
-            usedAlready.push(removeDuplicates[c]);
-        }
-
-        count++;
-        if (count == 1 && found == false) {
-            removeDuplicates.push(guessLetters[c]);
-        }
-        count = 0;
-        found = false;
-    }
+            randomImage();
 
 
-    // losesElement.textContent = losses;
+            // start the RIGHT LETTER FOR LOOP
+            for (var w = 0; w < songTitle.length; w++) {
 
-}
+                if (lettersGuessedElement == songTitle[w]) {
+                    correct = true;
+                    rightLetter.push(lettersGuessedElement);
+                    wins++;
+                }
 
- // ELSE for capturing ALL LETTERS and storing them in the GUESSLETTERS array
+                // Searching for duplicates
+
+                var count = 0;
+                var found = false;
 
 
- guessLetters.push(lettersGuessedElement);
- guessLettersElement.textContent = guessLetters.join(' , ');
+                for (r = 0; r < guessLetters.length; r++) {
+                    //console.log(guessLetters[i]);
+                    for (c = 0; c < removeDuplicates.length; c++) {
+                        if (guessLetters[c] == removeDuplicates[c]) {
+                            found = true;
+
+                        }
+
+                        usedAlready.push(removeDuplicates[c]);
+                    }
+
+                    count++;
+                    if (count == 1 && found == false) {
+                        removeDuplicates.push(guessLetters[c]);
+                    }
+                    count = 0;
+                    found = false;
+                }
 
 
+                // losesElement.textContent = losses;
+
+            }
+
+            // ELSE for capturing ALL LETTERS and storing them in the GUESSLETTERS array
+
+
+            guessLetters.push(lettersGuessedElement);
+            guessLettersElement.textContent = guessLetters.join(' , ');
+
+            losesElement.textContent = losses++;
+            guessesRemainingElement.textContent = guessesRemaining--;
+            winsElement.textContent = wins++;
