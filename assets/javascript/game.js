4257506096675c65c6aab530991820ae0746e425
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-02-26 01:20:39 -0600

    deleted photos...going to try again

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,30 @@
-// Create a giant wordguessgame object to hold all our logic and variables
+$(document).ready(function() {
+
+    // Create a giant wordguessgame object to hold all our logic and variables
 var wordGuessGame = {
     //Object Name : chosenWord along with info such as their picture and a song clip.
     chosenWord: {
+        
         taylorswift: {
-            picture: "assets/images/taylor-swift-album.png",
+            picture: "../images/taylor-swift-album.png",
             song: "Red",
             preview: "https://open.spotify.com/track/1RvQQGwaPipiNgz8RXAKA8?si=mx4Nzt97Q0eJs3xakLjP2A"
         },
 
         starset: {
-            picture: "assets/images/starset-monster-album.jpg",
+            picture: "../images/starset-monster-album.jpg",
             song: "Monster",
             preview: "https://open.spotify.com/track/0criiQKIY1hyU0lRbVhZ8L?si=5b7bDCDlQ0ebxgjH2f1F-A"
         },
 
         linkinpark: {
-            picture: "assets/images/linkin-park-what-ive-done-album.jpg",
+            picture: "../images/linkin-park-what-ive-done-album.jpg",
             song: "What I've Done",
             preview: "https://open.spotify.com/track/18lR4BzEs7e3qzc0KVkTpU?si=9_AMFULpRdmM4LIZ5qCs6w"
         },
 
         nickelback: {
-            picture: "assets/images/nickelback-silent-majority-album.jpg",
+            picture: "../images/nickelback-silent-majority-album.jpg",
             song: "Silent Majority",
             preview: "https://open.spotify.com/track/1LxMjTswgQqMjynxUdUO2g?si=7wxl3FLjQci3_unAKwcvuA"
         }
@@ -257,4 +260,8 @@ document.onkeyup = function (event) {
 
 
 };
+
+
+
+});
 
