bb73c6f67bf948ad542eb66cb3ca6aecb631711a
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-02-26 00:25:38 -0600

    updated the picture paths

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,25 +3,25 @@ var wordGuessGame = {
     //Object Name : chosenWord along with info such as their picture and a song clip.
     chosenWord: {
         taylorswift: {
-            picture: "../WORDGUESS/assets/images/taylor-swift-album.png",
+            picture: "assets/images/taylor-swift-album.png",
             song: "Red",
             preview: "https://open.spotify.com/track/1RvQQGwaPipiNgz8RXAKA8?si=mx4Nzt97Q0eJs3xakLjP2A"
         },
 
         starset: {
-            picture: "../WORDGUESS/assets/images/starset-monster-album.jpg",
+            picture: "assets/images/starset-monster-album.jpg",
             song: "Monster",
             preview: "https://open.spotify.com/track/0criiQKIY1hyU0lRbVhZ8L?si=5b7bDCDlQ0ebxgjH2f1F-A"
         },
 
         linkinpark: {
-            picture: "../WORDGUESS/assets/images/linkin-park-what-ive-done-album.jpg",
+            picture: "assets/images/linkin-park-what-ive-done-album.jpg",
             song: "What I've Done",
             preview: "https://open.spotify.com/track/18lR4BzEs7e3qzc0KVkTpU?si=9_AMFULpRdmM4LIZ5qCs6w"
         },
 
         nickelback: {
-            picture: "../WORDGUESS/assets/images/nickelback-silent-majority-album.jpg",
+            picture: "assets/images/nickelback-silent-majority-album.jpg",
             song: "Silent Majority",
             preview: "https://open.spotify.com/track/1LxMjTswgQqMjynxUdUO2g?si=7wxl3FLjQci3_unAKwcvuA"
         }
